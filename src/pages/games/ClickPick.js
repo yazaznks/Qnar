e62530f16abbd49d49ebbac9060d86a7df445c25
yazanks <y.shehab06@gmail.com>
2024-11-15 20:44:08 +0300

    full screen

--- src/pages/games/ClickPick.js
+++ src/pages/games/ClickPick.js
@@ -344,7 +344,7 @@ return () => {
     p: 3,
     borderRadius: 2,
     position: 'absolute',
-    bottom: 50,
+    top: 0,
     left: 0,
     width: '100%',
     height: '100%', // Adjust this as needed
